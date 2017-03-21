8a25ebbb7793ec2fe1bd1e5208d37a255f9b8e8f
Georgi Velikov <gmvelikov@gmail.com>
2017-03-21 23:43:37 +0200

    60 percent reached.

---
+++ Fancy/Fancy.Tests/Services/Common/MappingServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fancy.Services.Common;
+using AutoMapper;
+using Moq;
+
+namespace Fancy.Tests.Services.Common
+{
+    [TestClass]
+    public class MappingServiceTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_ShouldThrowArgumentNullException_WhenMapperIsNull()
+        {
+            // Arrange, Act & Assert
+            var mappingService = new MappingService(null);
+        }
+    }
+}
